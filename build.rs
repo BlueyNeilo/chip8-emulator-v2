b032f6e9955f566cbd173ef89752aa1b71680af3
Patrick Neilson <bluey.neilo@gmail.com>
2021-06-05 22:55:07 +1000

    :recycle: :truck: Refactor SDL2 build code and nest the library dirs

--- build.rs
+++ build.rs
@@ -5,36 +5,50 @@ use std::fs;
 fn main() {
     let target = env::var("TARGET").unwrap();
     if target.contains("pc-windows") {
-        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let lib_dir = get_sdl2_library_dir(&target, "lib");
-        let dll_dir = get_sdl2_library_dir(&target, "dll");
+        load_sdl2_lib(&target);
+        load_sdl2_dll(&target);
+    }
+}
 
-        println!("cargo:rustc-link-search=all={}", lib_dir.display());
-        for entry in fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
-            let entry_path = entry.expect("Invalid fs entry").path();
-            let file_name_result = entry_path.file_name();
-            let mut new_file_path = manifest_dir.clone();
-            if let Some(file_name) = file_name_result {
-                let file_name = file_name.to_str().unwrap();
-                if file_name.ends_with(".dll") {
-                    new_file_path.push(file_name);
-                    fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
-                }
+
+fn load_sdl2_lib(target: &str) {
+    let lib_dir = get_sdl2_library_dir(&target, "lib");
+    println!("cargo:rustc-link-search=all={}", lib_dir.display())
+}
+
+fn load_sdl2_dll(target: &str) {
+    let dll_dir = get_sdl2_library_dir(&target, "dll");
+
+    for entry in fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
+        let entry_path = entry.expect("Invalid fs entry").path();
+
+        // 'if let = [expr] && [expr]' is experimental, waiting for release
+        if let Some(extension) = entry_path.extension() { 
+            if extension == "dll" {
+            let destination_file_path = get_manifest_dir()
+                .join(entry_path.file_name()
+                    .expect("Invalid DLL file name"));
+            fs::copy(&entry_path, destination_file_path.as_path())
+                .expect("Can't copy from DLL dir");
             }
         }
     }
 }
 
 fn get_sdl2_library_dir(target: &str, dir_type: &str) -> PathBuf {
-    let mut dir_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    
-    let windows_compiler_dir = if target.contains("msvc") { "msvc" } else { "gnu-mingw" };
+    let win_compiler_dir = if target.contains("msvc") { "msvc" } else { "gnu-mingw" };
     let cpu_architecture_dir = if target.contains("x86_64") { "64" } else { "32" };
-    let path_list = vec![windows_compiler_dir, dir_type, cpu_architecture_dir];
 
-    for path in path_list.into_iter() {
-        dir_path.push(path);
-    }
+    let path_list = vec![
+        "sdl2-libs", 
+        win_compiler_dir, 
+        dir_type, 
+        cpu_architecture_dir
+    ];
 
-    dir_path
+    get_manifest_dir().join(path_list.iter().collect::<PathBuf>())
+}
+
+fn get_manifest_dir() -> PathBuf {
+    PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
 }
