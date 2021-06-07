a0553692abee6a95c30326bac338e8d24e1dd02d
Patrick Neilson <bluey.neilo@gmail.com>
2021-06-07 22:40:50 +1000

    :recycle: Refactor chip8 disassemble_code to use new code

--- src/chip8.rs
+++ src/chip8.rs
@@ -5,6 +5,7 @@ use sdl2::audio::{AudioDevice, AudioStatus, AudioCallback};
 
 use rng::rng_byte;
 use constants::{W, H, N};
+use opcode::{Disassembler, OpcodeDisassembler};
 
 #[allow(non_snake_case)]
 pub struct Chip8 {
@@ -64,79 +65,14 @@ impl Chip8 {
     pub fn disassemble_code(&mut self, memory: &[u8; 0x1000]) {
         println!("Disassembling code: \n");
 
-        //let mut dfilebuf = BufWriter::new(File::create("hex.txt").unwrap());
-
         let mut addr: usize = 0x200;
         let mut opcode: u16 = (memory[addr] as u16) << 8 | (memory[addr + 1] as u16);
         while opcode!=0 {
-            //Log to the assembly file
-            //let opform = format!("{:04X}\n",opcode);
-            //let opbuf = opform.as_bytes();
-            //dfilebuf.write(&opbuf);
-
-            //Decode Opcode
-            let u = opcode >> 12; //the first nibble in the instruction
-            let nnn = opcode & 0xFFF; //address
-            let nn = opcode & 0xFF; //8 bit constant (byte)
-            let n = opcode & 0xF; //4 bit constant (last nibble)
-            let x = nnn >> 8; //the second nibble in the instruction (when applicable)
-            let y = nn >> 4; // the third nibble in the instruction (when applicable)
-
-            //Execute Opcode
-            match opcode {
-                0x00E0 => println!("{:03x}: {:04X} 'CLS'",addr,opcode), //CLS; clears the display
-                0x00EE => println!("{:03x}: {:04X} 'RET'",addr,opcode), //RET; return from a subroutine
-                _ => match u {
-                    //0x0 => {}, //SYS addr; 0NNN, Calls RCA 1802 program at address NNN. not used for this emulator
-                    0x1 => println!("{:03x}: {:04X} 'JP ${:03x}'",addr,opcode,nnn), //JP addr; 1NNN, Goto NNN
-                    0x2 => println!("{:03x}: {:04X} 'CALL ${:03x}'",addr,opcode,nnn), //CALL addr; 2NNN, Call subroutine at NNN
-                    0x3 => println!("{:03x}: {:04X} 'SE V{:X}, {}'",addr,opcode,x,nn), //SE Vx, byte; 3XNN, skip if Vx==NN
-                    0x4 => println!("{:03x}: {:04X} 'SNE V{:X}, {}'",addr,opcode,x,nn), //SNE Vx, byte; 4XNN, skip if Vx!=NN
-                    0x5 => if n==0 {println!("{:03x}: {:04X} 'SE V{:X}, V{:X}'",addr,opcode,x,y) //SE Vx, Vy; 5XY0, skip if Vx==Vy
-                        } else {println!("Invalid opcode: {:04X}",opcode)},
-                    0x6 => println!("{:03x}: {:04X} 'LD V{:X}, {}'",addr,opcode,x,nn), //LD Vx, byte; 6XNN, Vx = NN (assignment)
-                    0x7 => println!("{:03x}: {:04X} 'ADD V{:X}, {}'",addr,opcode,x,nn), //ADD Vx, byte; 7XNN, Vx += NN (addition)
-                    0x8 => match n { //8XYn
-                        0x0 => println!("{:03x}: {:04X} 'LD V{:X}, V{:X}'",addr,opcode,x,y), //LD Vx, Vy; 8XY0, Vx = Vy
-                        0x1 => println!("{:03x}: {:04X} 'OR V{:X}, V{:X}'",addr,opcode,x,y), //OR Vx, Vy; 8XY1, Vx = Vx|Vy (OR)
-                        0x2 => println!("{:03x}: {:04X} 'AND V{:X}, V{:X}'",addr,opcode,x,y), //AND Vx, Vy; 8XY2, Vx = Vx&Vy (AND)
-                        0x3 => println!("{:03x}: {:04X} 'XOR V{:X}, V{:X}'",addr,opcode,x,y), //XOR Vx, Vy; 8XY3, Vx = Vx^Vy (XOR)
-                        0x4 => println!("{:03x}: {:04X} 'ADD V{:X}, V{:X}'",addr,opcode,x,y), //ADD Vx, Vy; 8XY4, Vx += Vy
-                        0x5 => println!("{:03x}: {:04X} 'SUB V{:X}, V{:X}'",addr,opcode,x,y), //SUB Vx, Vy; 8XY5, Vx -= Vy
-                        0x6 => println!("{:03x}: {:04X} 'SHR V{:X}'",addr,opcode,x), //SHR Vx; 8XY6, Vx >> 1
-                        0x7 => println!("{:03x}: {:04X} 'SUBN V{:X}, V{:X}'",addr,opcode,x,y), //SUBN Vx, Vy; 8XY7, Vx=Vy-Vx
-                        0xE => println!("{:03x}: {:04X} 'SHL V{:X}'",addr,opcode,x), //SHL Vx; 8XYE, Vx << 1
-                        _ => println!("Invalid opcode: {:04X}",opcode)
-                    },
-                    0x9 => println!("{:03x}: {:04X} 'SNE V{:X}, V{:X}'",addr,opcode,x,y), //SNE Vx, Vy; 9XY0, skip if Vx!=Vy
-                    0xA => println!("{:03x}: {:04X} 'LD I, {:X}'",addr,opcode,nnn), //LD I, addr; ANNN, I=NNN
-                    0xB => println!("{:03x}: {:04X} 'JP V0, {:X}'",addr,opcode,nnn), //JP V0, addr; BNNN, PC = V0 + NNN
-                    0xC => println!("{:03x}: {:04X} 'RND V{:X}, {:b}'",addr,opcode,x,nn), //RND Vx, byte; CXNN, Vx = rand() & NN
-                    0xD => println!("{:03x}: {:04X} 'DRW V{:X}, V{:X}, {}'",addr,opcode,x,y,n), //DRW Vx, Vy, nibble; DXYN, Display n-byte sprite starting at memory location I at (Vx, Vy)
-                    0xE => match nn {
-                        0x9E => println!("{:03x}: {:04X} 'SKP V{:X}'",addr,opcode,x), //SKP Vx; EX9E, Skip next instruction if key with the value of Vx is pressed.
-                        0xA1 => println!("{:03x}: {:04X} 'SKNP V{:X}'",addr,opcode,x), //SKNP Vx; EXA1, Skip next instruction if key with the value of Vx is not pressed.
-                        _ => println!("Invalid opcode: {:04X}", opcode)
-                    },
-                    0xF => match nn { //FXzz
-                        0x07 => println!("{:03x}: {:04X} 'LD V{:X}, DT'",addr,opcode,x), //LD Vx, DT; FX07 Set Vx = delay timer value.
-                        0x0A => println!("{:03x}: {:04X} 'LD V{:X}, K'",addr,opcode,x), //LD Vx, K; FX0A, Wait for a key press, store the value of the key in Vx.
-                        0x15 => println!("{:03x}: {:04X} 'LD DT, V{:X}'",addr,opcode,x), //LD DT, Vx; FX15, Set delay timer = Vx.
-                        0x18 => println!("{:03x}: {:04X} 'LD ST, V{:X}'",addr,opcode,x), //LD ST, Vx; FX18, Set sound timer = Vx.
-                        0x1E => println!("{:03x}: {:04X} 'ADD I, V{:X}'",addr,opcode,x), //ADD I, Vx; FX1E I += Vx
-                        0x29 => println!("{:03x}: {:04X} 'LD F, V{:X}'",addr,opcode,x), //LD F, Vx; FX29 I=sprite_addr[Vx]
-                        0x33 => println!("{:03x}: {:04X} 'LD B, V{:X}'",addr,opcode,x), //LD B, Vx; FX33 set_BCD(Vx)
-                        0x55 => println!("{:03x}: {:04X} 'LD [I], V{:X}'",addr,opcode,x), //LD [I], Vx; LD [I], Vx; FX55 reg_dump(Vx, &I)
-                        0x65 => println!("{:03x}: {:04X} 'LD V{:X}, [I]'",addr,opcode,x), //LD Vx, [I]; FX65 reg_load(Vx,&I)
-                        _ => println!("Invalid opcode: {:04X}", opcode)
-                    },
-                    _ => println!("Invalid opcode: {:04X}", opcode)
-                }
-            };
+            let disassembled = OpcodeDisassembler::disassemble(opcode);
+            println!("{:03x}: {:04X} '{}'", addr, opcode, disassembled);
             addr+=2;
             opcode = (memory[addr] as u16) << 8 | (memory[addr + 1] as u16)
         }
-        //dfilebuf.flush();
     }
 
     pub fn emulate_cycle(&mut self, memory: &mut [u8; 0x1000], pixels: &mut [bool; N], key: &[bool; 0x10], device: &AudioDevice<impl AudioCallback>) {
