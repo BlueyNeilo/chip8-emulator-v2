4769a388510813af48fd411e53a9851a21537606
Patrick Neilson <bluey.neilo@gmail.com>
2021-06-08 14:32:44 +1000

    :recycle: :construction: Start refactoring opcode execution in chip8

--- src/chip8.rs
+++ src/chip8.rs
@@ -74,107 +74,115 @@ impl Chip8 {
             opcode = (memory[addr] as u16) << 8 | (memory[addr + 1] as u16)
         }
     }
+    
+    fn fetch_instruction(&mut self, bytes: &[u8]) -> u16 {
+        (bytes[0] as u16) << 8 | bytes[1] as u16
+    }
 
     pub fn emulate_cycle(&mut self, memory: &mut [u8; 0x1000], pixels: &mut [bool; N], key: &[bool; 0x10], device: &AudioDevice<impl AudioCallback>) {
         //Fetch Opcode
-        let opcode: u16 = (memory[self.pc as usize] as u16) << 8 | (memory[(self.pc as usize) + 1] as u16);
+        let pc = self.pc as usize;
+        let instruction: u16 = self.fetch_instruction(&memory[pc..pc+2]);
         self.pc += 2;
 
         //Decode Opcode
-        let u = opcode >> 12; //the first nibble in the instruction
-        let nnn = opcode & 0xFFF; //address
-        let nn: u8 = (opcode & 0xFF) as u8; //8 bit constant (byte)
-        let n = opcode & 0xF; //4 bit constant (last nibble)
+        let opcode: Opcode = OpcodeDisassembler::disassemble(instruction);
+        let u = instruction >> 12; //the first nibble in the instruction
+        let nnn = instruction & 0xFFF; //address
+        let nn: u8 = (instruction & 0xFF) as u8; //8 bit constant (byte)
+        let n = instruction & 0xF; //4 bit constant (last nibble)
         let x: usize = (nnn as usize) >> 8; //the second nibble in the instruction (when applicable)
         let y: usize = (nn as usize) >> 4; // the third nibble in the instruction (when applicable)
         #[allow(non_snake_case)]
         let I: usize = self.I as usize;
-        //Execute Opcode
+
         match opcode {
-            0x00E0 => {self.clear_display()}, //CLS; clears the display
-            0x00EE => {self.pc = self.stack[self.sp as usize]; self.sp-=1}, //RET; return from a subroutine
-            _ => match u {
-                //0x0 => {}, //SYS addr; 0NNN, Calls RCA 1802 program at address NNN. not used for this emulator
-                0x1 => {self.pc = nnn}, //JP addr; 1NNN, Goto NNN
-                0x2 => {self.sp+=1; self.stack[self.sp as usize]=self.pc; self.pc = nnn}, //CALL addr; 2NNN, Call subroutine at NNN
-                0x3 => {if self.reg_v[x]==nn {self.pc+=2}}, //SE Vx, byte; 3XNN, skip if Vx==NN
-                0x4 => {if self.reg_v[x]!=nn {self.pc+=2}}, //SNE Vx, byte; 4XNN, skip if Vx!=NN
-                0x5 => if n==0 {
-                        if self.reg_v[x]==self.reg_v[y] {self.pc+=2} //SE Vx, Vy; 5XY0, skip if Vx==Vy
-                    } else {panic!("Invalid opcode: {:04X} at {:04x}", opcode, self.pc-2)},
-                0x6 => {self.reg_v[x]=nn}, //LD Vx, byte; 6XNN, Vx = NN (assignment)
-                0x7 => {self.reg_v[x]=((self.reg_v[x] as u16)+(nn as u16)) as u8}, //ADD Vx, byte; 7XNN, Vx += NN (addition)
-                0x8 => match n { //8XYn
-                    0x0 => {self.reg_v[x]=self.reg_v[y]}, //LD Vx, Vy; 8XY0, Vx = Vy
-                    0x1 => {self.reg_v[x]|=self.reg_v[y]}, //OR Vx, Vy; 8XY1, Vx = Vx|Vy (OR)
-                    0x2 => {self.reg_v[x]&=self.reg_v[y]}, //AND Vx, Vy; 8XY2, Vx = Vx&Vy (AND)
-                    0x3 => {self.reg_v[x]^=self.reg_v[y]}, //XOR Vx, Vy; 8XY3, Vx = Vx^Vy (XOR)
-                    0x4 => {self.reg_v[0xF]=(((self.reg_v[x] as u16)+(self.reg_v[y] as u16))>>8) as u8; self.reg_v[x]=(((self.reg_v[x] as u16)+(self.reg_v[y] as u16))&0xFF) as u8}, //ADD Vx, Vy; 8XY4, Vx += Vy
-                    0x5 => {self.reg_v[0xF]=(self.reg_v[x]>self.reg_v[y]) as u8; self.reg_v[x]=((self.reg_v[x] as i16)-(self.reg_v[y] as i16)) as u8}, //SUB Vx, Vy; 8XY5, Vx -= Vy
-                    0x6 => {self.reg_v[0xF]=self.reg_v[x]&0x1; self.reg_v[x]=self.reg_v[x] >> 1}, //SHR Vx; 8XY6, Vx >> 1
-                    0x7 => {self.reg_v[0xF]=(self.reg_v[y]>self.reg_v[x]) as u8; self.reg_v[x]=((self.reg_v[y] as i16)-(self.reg_v[x] as i16)) as u8}, //SUBN Vx, Vy; 8XY7, Vx=Vy-Vx
-                    0xE => {self.reg_v[0xF]=self.reg_v[x]>>7; self.reg_v[x]=self.reg_v[x] << 1}, //SHL Vx; 8XYE, Vx << 1
-                    _ => panic!("Invalid opcode: {:04X} at {:04x}", opcode, self.pc-2)
-                },
-                0x9 => {if self.reg_v[x]!=self.reg_v[y] {self.pc+=2}}, //SNE Vx, Vy; 9XY0, skip if Vx!=Vy
-                0xA => {self.I=nnn}, //LD I, addr; ANNN, I=NNN
-                0xB => {self.pc=(self.reg_v[0] as u16)+nnn}, //JP V0, addr; BNNN, PC = V0 + NNN (Error handling could be added)
-                0xC => {self.reg_v[x]=rng_byte()&nn}, //RND Vx, byte; CXNN, Vx = rand() & NN
-                0xD => {
-                    /*The interpreter reads n bytes from memory, starting at the address stored in I.
-                    These bytes are then displayed as sprites on screen at coordinates (Vx, Vy).
-                    Sprites are XORed onto the existing screen.
-                    If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0.
-                    If the sprite is positioned so part of it is outside the coordinates of the display,
-                    it wraps around to the opposite side of the screen. */
-                    self.reg_v[0xF]=0;
-                    let mut px: usize = self.reg_v[x] as usize;
-                    let mut py: usize = self.reg_v[y] as usize;
-                    for i in 0..n {
-                        while py>=H {py-=H};
-                        for ii in 0..8 {
-                            while px>=W {px-=W};
-                            self.update_pixel(pixels, px, py, (((memory[I+(i as usize)]>>(7-ii))&1)==1) as bool);
-                            px+=1
+            Opcode(CLS, NONE) => self.clear_display(),
+            _ => match instruction {
+                0x00EE => {self.pc = self.stack[self.sp as usize]; self.sp-=1}, //RET; return from a subroutine
+                _ => match u {
+                    //0x0 => {}, //SYS addr; 0NNN, Calls RCA 1802 program at address NNN. not used for this emulator
+                    0x1 => {self.pc = nnn}, //JP addr; 1NNN, Goto NNN
+                    0x2 => {self.sp+=1; self.stack[self.sp as usize]=self.pc; self.pc = nnn}, //CALL addr; 2NNN, Call subroutine at NNN
+                    0x3 => {if self.reg_v[x]==nn {self.pc+=2}}, //SE Vx, byte; 3XNN, skip if Vx==NN
+                    0x4 => {if self.reg_v[x]!=nn {self.pc+=2}}, //SNE Vx, byte; 4XNN, skip if Vx!=NN
+                    0x5 => if n==0 {
+                            if self.reg_v[x]==self.reg_v[y] {self.pc+=2} //SE Vx, Vy; 5XY0, skip if Vx==Vy
+                        } else {panic!("Invalid opcode: {:04X} at {:04x}", instruction, self.pc-2)},
+                    0x6 => {self.reg_v[x]=nn}, //LD Vx, byte; 6XNN, Vx = NN (assignment)
+                    0x7 => {self.reg_v[x]=((self.reg_v[x] as u16)+(nn as u16)) as u8}, //ADD Vx, byte; 7XNN, Vx += NN (addition)
+                    0x8 => match n { //8XYn
+                        0x0 => {self.reg_v[x]=self.reg_v[y]}, //LD Vx, Vy; 8XY0, Vx = Vy
+                        0x1 => {self.reg_v[x]|=self.reg_v[y]}, //OR Vx, Vy; 8XY1, Vx = Vx|Vy (OR)
+                        0x2 => {self.reg_v[x]&=self.reg_v[y]}, //AND Vx, Vy; 8XY2, Vx = Vx&Vy (AND)
+                        0x3 => {self.reg_v[x]^=self.reg_v[y]}, //XOR Vx, Vy; 8XY3, Vx = Vx^Vy (XOR)
+                        0x4 => {self.reg_v[0xF]=(((self.reg_v[x] as u16)+(self.reg_v[y] as u16))>>8) as u8; self.reg_v[x]=(((self.reg_v[x] as u16)+(self.reg_v[y] as u16))&0xFF) as u8}, //ADD Vx, Vy; 8XY4, Vx += Vy
+                        0x5 => {self.reg_v[0xF]=(self.reg_v[x]>self.reg_v[y]) as u8; self.reg_v[x]=((self.reg_v[x] as i16)-(self.reg_v[y] as i16)) as u8}, //SUB Vx, Vy; 8XY5, Vx -= Vy
+                        0x6 => {self.reg_v[0xF]=self.reg_v[x]&0x1; self.reg_v[x]=self.reg_v[x] >> 1}, //SHR Vx; 8XY6, Vx >> 1
+                        0x7 => {self.reg_v[0xF]=(self.reg_v[y]>self.reg_v[x]) as u8; self.reg_v[x]=((self.reg_v[y] as i16)-(self.reg_v[x] as i16)) as u8}, //SUBN Vx, Vy; 8XY7, Vx=Vy-Vx
+                        0xE => {self.reg_v[0xF]=self.reg_v[x]>>7; self.reg_v[x]=self.reg_v[x] << 1}, //SHL Vx; 8XYE, Vx << 1
+                        _ => panic!("Invalid opcode: {:04X} at {:04x}", instruction, self.pc-2)
+                    },
+                    0x9 => {if self.reg_v[x]!=self.reg_v[y] {self.pc+=2}}, //SNE Vx, Vy; 9XY0, skip if Vx!=Vy
+                    0xA => {self.I=nnn}, //LD I, addr; ANNN, I=NNN
+                    0xB => {self.pc=(self.reg_v[0] as u16)+nnn}, //JP V0, addr; BNNN, PC = V0 + NNN (Error handling could be added)
+                    0xC => {self.reg_v[x]=rng_byte()&nn}, //RND Vx, byte; CXNN, Vx = rand() & NN
+                    0xD => {
+                        /*The interpreter reads n bytes from memory, starting at the address stored in I.
+                        These bytes are then displayed as sprites on screen at coordinates (Vx, Vy).
+                        Sprites are XORed onto the existing screen.
+                        If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0.
+                        If the sprite is positioned so part of it is outside the coordinates of the display,
+                        it wraps around to the opposite side of the screen. */
+                        self.reg_v[0xF]=0;
+                        let mut px: usize = self.reg_v[x] as usize;
+                        let mut py: usize = self.reg_v[y] as usize;
+                        for i in 0..n {
+                            while py>=H {py-=H};
+                            for ii in 0..8 {
+                                while px>=W {px-=W};
+                                self.update_pixel(pixels, px, py, (((memory[I+(i as usize)]>>(7-ii))&1)==1) as bool);
+                                px+=1
+                            };
+                            px=self.reg_v[x] as usize;
+                            py+=1
                         };
-                        px=self.reg_v[x] as usize;
-                        py+=1
-                    };
-                }, //DRW Vx, Vy, nibble; DXYN, Display n-byte sprite starting at memory location I at (Vx, Vy)
-                0xE => match nn {
-                    0x9E => {if key[(self.reg_v[x]&0xF) as usize] {self.pc+=2}}, //SKP Vx; EX9E, Skip next instruction if key with the value of Vx is pressed.
-                    0xA1 => {if !key[(self.reg_v[x]&0xF) as usize] {self.pc+=2}}, //SKNP Vx; EXA1, Skip next instruction if key with the value of Vx is not pressed.
-                    _ => panic!("Invalid opcode: {:04X} at {:04x}", opcode, self.pc-2)
-                },
-                0xF => match nn { //FXzz
-                    0x07 => {self.reg_v[x]=self.delay_timer}, //LD Vx, DT; FX07 Set Vx = delay timer value.
-                    0x0A => {self.key_wait=true; self.reg_wait=x as usize}, //LD Vx, K; FX0A, Wait for a key press, store the value of the key in Vx.
-                    0x15 => {self.delay_timer=self.reg_v[x]}, //LD DT, Vx; FX15, Set delay timer = Vx.
-                    0x18 => {self.sound_timer=self.reg_v[x]}, //LD ST, Vx; FX18, Set sound timer = Vx.
-                    0x1E => {self.reg_v[0xF]=((self.I+(self.reg_v[x] as u16))>>12) as u8; self.I=(self.I+(self.reg_v[x] as u16))&0xFFF}, //ADD I, Vx; FX1E I += Vx
-                    0x29 => {self.I=(0x50+(0x5*self.reg_v[x])) as u16}, //LD F, Vx; FX29 I=sprite_addr[Vx]
-                    0x33 => {
-                        /*The interpreter takes the decimal value of Vx,
-                        and places the hundreds digit in memory at location in I,
-                        the tens digit at location I+1,
-                        and the ones digit at location I+2.*/
-                        memory[I] = (self.reg_v[x]/10)/10; //hundreds
-                        memory[I+1] = (self.reg_v[x]/10)%10; //tens
-                        memory[I+2] = self.reg_v[x]%10 //ones
-                    }, //LD B, Vx; FX33 set_BCD(Vx)
-                    0x55 => {
-                        for i in 0..(x+1) {
-                            memory[I+i] = self.reg_v[i]
-                        }
-                    }, //LD [I], Vx; LD [I], Vx; FX55 reg_dump(Vx, &I)
-                    0x65 => {
-                        for i in 0..(x+1) {
-                            self.reg_v[i] = memory[I+i]
-                        }
-                    }, //LD Vx, [I]; FX65 reg_load(Vx,&I)
-                    _ => panic!("Invalid opcode: {:04X} at {:04x}", opcode, self.pc-2)
-                },
-                _ => panic!("Invalid opcode: {:04X} at {:04x}", opcode, self.pc-2)
+                    }, //DRW Vx, Vy, nibble; DXYN, Display n-byte sprite starting at memory location I at (Vx, Vy)
+                    0xE => match nn {
+                        0x9E => {if key[(self.reg_v[x]&0xF) as usize] {self.pc+=2}}, //SKP Vx; EX9E, Skip next instruction if key with the value of Vx is pressed.
+                        0xA1 => {if !key[(self.reg_v[x]&0xF) as usize] {self.pc+=2}}, //SKNP Vx; EXA1, Skip next instruction if key with the value of Vx is not pressed.
+                        _ => panic!("Invalid opcode: {:04X} at {:04x}", instruction, self.pc-2)
+                    },
+                    0xF => match nn { //FXzz
+                        0x07 => {self.reg_v[x]=self.delay_timer}, //LD Vx, DT; FX07 Set Vx = delay timer value.
+                        0x0A => {self.key_wait=true; self.reg_wait=x as usize}, //LD Vx, K; FX0A, Wait for a key press, store the value of the key in Vx.
+                        0x15 => {self.delay_timer=self.reg_v[x]}, //LD DT, Vx; FX15, Set delay timer = Vx.
+                        0x18 => {self.sound_timer=self.reg_v[x]}, //LD ST, Vx; FX18, Set sound timer = Vx.
+                        0x1E => {self.reg_v[0xF]=((self.I+(self.reg_v[x] as u16))>>12) as u8; self.I=(self.I+(self.reg_v[x] as u16))&0xFFF}, //ADD I, Vx; FX1E I += Vx
+                        0x29 => {self.I=(0x50+(0x5*self.reg_v[x])) as u16}, //LD F, Vx; FX29 I=sprite_addr[Vx]
+                        0x33 => {
+                            /*The interpreter takes the decimal value of Vx,
+                            and places the hundreds digit in memory at location in I,
+                            the tens digit at location I+1,
+                            and the ones digit at location I+2.*/
+                            memory[I] = (self.reg_v[x]/10)/10; //hundreds
+                            memory[I+1] = (self.reg_v[x]/10)%10; //tens
+                            memory[I+2] = self.reg_v[x]%10 //ones
+                        }, //LD B, Vx; FX33 set_BCD(Vx)
+                        0x55 => {
+                            for i in 0..(x+1) {
+                                memory[I+i] = self.reg_v[i]
+                            }
+                        }, //LD [I], Vx; LD [I], Vx; FX55 reg_dump(Vx, &I)
+                        0x65 => {
+                            for i in 0..(x+1) {
+                                self.reg_v[i] = memory[I+i]
+                            }
+                        }, //LD Vx, [I]; FX65 reg_load(Vx,&I)
+                        _ => panic!("Invalid opcode: {:04X} at {:04x}", instruction, self.pc-2)
+                    },
+                    _ => panic!("Invalid opcode: {:04X} at {:04x}", instruction, self.pc-2)
+                }
             }
         };
 
